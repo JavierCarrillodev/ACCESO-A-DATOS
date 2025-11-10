a4006dc162e2ac08693ef0ce1edf9bdf9e48b2fb
Javier <carriloruiz@gmail.com>
2025-11-10 16:15:59 +0100

    Actualizacion Acceso a datos

---
+++ Tema 2/Curso Spring Boot/cursoSpringboot/src/main/java/cursoSpringboot/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package cursoSpringboot.controllers;
+
+import cursoSpringboot.domain.Product;
+import cursoSpringboot.service.ProductsServicelmpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductController {
+    ProductsServicelmpl productsServicelmpl = new ProductsServicelmpl();
+
+    @GetMapping
+    public ResponseEntity<?> getProducts(){
+        List<Product> products = productsServicelmpl.getProducts();
+
+        return ResponseEntity.ok(products);
+    }
+
+}
